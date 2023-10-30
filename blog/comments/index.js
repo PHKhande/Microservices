084c46abdb176710f94aef52adc8dc511ec132c4
PHKhande <phk151raj@gmail.com>
2023-10-30 21:58:21 +0530

    Updating Comments part

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,7 +1,9 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { randomBytes } = require('crypto');
 
+app.use(cors());
 app.use(bodyParser.json());
 const commentsByPostsId = {};
 
