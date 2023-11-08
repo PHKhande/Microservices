6564370c972f489da28339ee8a6ec5a32fe5e74d
PHKhande <phk151raj@gmail.com>
2023-11-08 21:57:04 +0530

    Emitting and recieving events

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -9,10 +9,12 @@ app.post( '/events', (req, res) => {
     
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    // axios.post('http://localhost:4002/events', event);
 
     res.send({status : 'OK'});
 
 });
 
-app.listen(4005);
+app.listen(4005, () => {
+    console.log("Listening on 4005");
+  });
--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,7 @@ const app = require("express")();
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -11,11 +12,21 @@ app.get("/posts", (req, res, next) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res, next) => {
+app.post("/posts", async (req, res, next) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = { id, title };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: { id, title },
+  });
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("EVENT RECIEVED: ", req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => {
